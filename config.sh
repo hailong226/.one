b4d2ea44fc1d766035d5e27cc8fec5c85842322c
hailong <hailong15226@gmail.com>
2017-01-03 19:55:00 +0800

    add config.sh

--- config.sh
+++ config.sh
@@ -7,5 +7,8 @@ ln ~/.one/.tmux.conf ~/.tmux.conf
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln ~/.one/.vimrc ~/.vimrc
 
+curl -O https://raw.githubusercontent.com/vince67/v7_config/master/vim.sh
+bash vim.sh
+
 # configuration zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
